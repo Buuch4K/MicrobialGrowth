1d6d10113c3dd2aacb50373db021e13fe4e40528
Jonas <jonas.guler@uzh.ch>
2022-02-15 11:22:02 +0100

    Debugging find_zero and add likelyhood

--- BasicModel.jl
+++ BasicModel.jl
@@ -1,40 +1,55 @@
-using Roots, AdvancedMH, Distributions, Random
-Random.seed!(7)
+using Roots, RRRMC, Distributions, Random
+# Random.seed!(4)
 
-function find_division(k,x)
+function find_division(k,size_of_cell)
     function f(t)
         #= 
             In this function we have a relation of the division time and the survival probability.
             we draw a random k between 0 and 1 and then solve for t
         =#
-    
-        log(k) - omega_2*v*t/(v+u) - (omega_2*x)/(omega_1*(v+u))*exp(omega_1*t) 
+        return log(k) + omega_2*v*t/(v+u) + (omega_2*size_of_cell)/(omega_1*(v+u))*exp(omega_1*t) 
     end
-    
-    return find_zero(f,(0,Inf))
+    fx = ZeroProblem(f, 1)
+    return solve(fx)
 end
 
+
+function compute_likelyhood(time)
+    #=
+        This function computes the liklyhood for an observation Y given the initial parameters (theta, x0) 
+    =#
+    like = 1;
+    for t in time
+        temp = (omega_2*v/(v+u) + omega_2*x0/(v+u)*exp(omega_1*t)) * exp(- omega_2*v*t/(v+u) - (omega_2*x0)/(omega_1*(v+u))*exp(omega_1*t))
+        like = like * temp
+    end
+    return like
+end
 
 
 N = 10; #number of observations
-x0 = 0.1; #initial size
+x0 = 5; #initial size
 
-d = LogNormal(0,1)
-u = rand(d, 1); #lower treshhold for division
-v = rand(d,1); #upper treshhold for division
-omega_1 = rand(d,1); #exponential growth rate
-omega_2 = rand(d,1); #hazard rate functions constant
+d = LogNormal(log(2)-1^2/2,1);
+u = 6.689; #lower treshhold for division
+v = 8.977 #upper treshhold for division
+omega_1 = 1; #exponential growth rate
+omega_2 = 0.302; #hazard rate functions constant
 
-
-X = [x0];
-Y_obs = [];
+X = Float64[x0]; #sizes of the cells
+Y_obs = Float64[]; #division times
 
 for n = 1:N
-    k = rand(1);
+    k = rand(Uniform(0,1),1)[1]
     push!(Y_obs, find_division(k,X[n]))
-    X[n+1] = X[n]/2*exp(omega_1*Y_obs[n])
+    next_size = X[n]/2 * exp(omega_1*Y_obs[n])
+    push!(X, next_size)
 end
-println(X,Y_obs)
+
+println(X)
+println(Y_obs)
+
+likelyhood = compute_likelyhood(Y_obs)
 
 
 
--- playground.jl
+++ playground.jl
@@ -1,1 +1,11 @@
-println("Hello World")
+using Roots
+
+f(x) = exp(x)-x^4
+cand = 8
+fx = ZeroProblem(f,cand)
+
+x0 = find_zero(f,(8,9))
+
+x1 = solve(fx)
+println("One root is $x0")
+println("Another method yields to the root $x1")
