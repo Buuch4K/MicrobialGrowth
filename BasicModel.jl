18eddf75042318140a2a1d031b4591dc403acd61
Jonas <jonas.guler@uzh.ch>
2022-02-24 16:25:54 +0100

    debugging find_division und log_lokeli

--- BasicModel.jl
+++ BasicModel.jl
@@ -1,4 +1,4 @@
-using Roots, Distributions, Random, Statistics, StatsPlots
+using Roots, Distributions, Random, Statistics, StatsPlots, Plots
 include("./SimpleMCMC.jl")
 using .SimpleMCMC
 # Random.seed!(4)
@@ -9,44 +9,65 @@ function generate_data(size_of_cell,N)
         This function generates a data set of length N for initial size "size_of_cell"
         output:
             X: vector of all sizes at division time (x0,x1,...,xN)
-            Y_obs: vector of all observed division times (tau1,...,tauN)
+            Y: vector of all observed division times (tau1,...,tauN)
             init_para: initial values of all parameters
     =#
-    function find_division(k,s,o1,o2,lb,ub)
-        #=
-            find_division computes the division time for a specific cell cycle
-            input:
-                k: random number in [0,1]
-                s: size of the cell after previous division
-                o1: initial value for omega1
-                02: initial value for omega2
-                lb: initial value for u
-                ub: initial value for v
-            output:
-                division time for this cell cycle
-        =#
-        function f(t)
-            return log(k) + o2*ub*t/(ub+lb) + (o2*s)/(o1*(ub+lb))*exp(o1*t) - (o2*s)/(o1*(ub+lb))
-        end
-        fx = ZeroProblem(f, 1)
-        return solve(fx)
-    end
-
     u_init = 0.6; #lower treshhold for division
-    v_init = 1; #upper treshhold for division
-    omega1_init = 1; #exponential growth rate
+    v_init = 1.0; #upper treshhold for division
+    omega1_init = 1.0; #exponential growth rate
     omega2_init = 0.5; #hazard rate functions constant    
     init_para = [omega1_init,omega2_init,u_init,v_init];
     
-    X = Float64[size_of_cell]; #sizes of the cell after division
-    Y_obs = Float64[]; #division times
+    X = Float64[size_of_cell]; #sizes of the cell at division
+    Y = Float64[]; #division times
+    k = rand(Uniform(0,1),N)
     for n = 1:N
-        k = rand(Uniform(0,1),1)[1]
-        push!(Y_obs, find_division(k,X[n],omega1_init,omega2_init,u_init,v_init))
-        next_size = X[n]/2 * exp(omega1_init*Y_obs[n])
+        push!(Y, find_division(k[n],X[n],omega1_init,omega2_init,u_init,v_init))
+        next_size = X[n]/2 * exp(omega1_init*Y[n])
         push!(X, next_size)
     end
-    return (X, Y_obs, init_para)
+    return (X, Y, init_para)
+end
+
+
+function find_division(k,s,o1,o2,lb,ub)
+    #=
+        find_division computes the division time for a specific cell cycle
+        input:
+            k: random number in [0,1]
+            s: size of the cell at previous division
+            o1: initial value for omega1
+            02: initial value for omega2
+            lb: initial value for u
+            ub: initial value for v
+        output:
+            division time for this cell cycle
+    =#
+    function f(t)
+        if s < lb
+            return log(k) + o2/(ub+lb)*(lb/o1*exp(o1*t) + ub*t - lb/o1)
+        else
+            return log(k) + o2/(ub+lb)*(s/o1*exp(o1*t) + ub*t - s/o1)
+        end
+    end
+    fx = ZeroProblem(f, 1)
+    return solve(fx) + max(0,1/o1*log(lb/s))
+end
+
+
+function plot_survival(t,s,o1,o2,lb,ub)
+    result = Array{Float64}(undef,length(t));
+    if s < lb
+        for k = 1:length(t)
+            temp = exp(-o2/(ub+lb)*(lb/o1*exp(o1*t[k]) + ub*t[k] - lb/o1 - ub*1/o1*log(lb/s)))
+            result[k] = min(1,temp)
+        end
+    else
+        for k = 1:length(t)
+            result[k] = exp(-o2/(ub+lb)*(s/o1*exp(o1*t[k]) + ub*t[k] - s/o1))
+        end
+    end
+    scatter(t,result)
 end
 
 
@@ -56,7 +77,8 @@ function log_likeli(time,size,o1,o2,lb,ub)
     =#
     like = 0.0;
     for k in 1:length(time)
-        temp = (o2*ub/(ub+lb) + o2*size[k]/(ub+lb)*exp(o1*time[k])) * exp(- o2*ub*time[k]/(ub+lb) - o2*size[k]/(o1*(ub+lb))*exp(o1*time[k]) + o2*size[k]/(o1*(ub+lb)))
+        t0 = max(0,1/o1*log(lb/size[k]))
+        temp = (o2/(ub+lb)) * (ub + size[k]*exp(o1*time[k])) * exp((-o2/(ub+lb))*(size[k]/o1*exp(o1*time[k]) - size[k]/o1*exp(o1*t0) + ub*time[k] - ub*t0))
         like += log(temp)
     end
     return like
@@ -68,22 +90,28 @@ x0 = 0.725; #initial size
 
 #generating the first dataset
 size, div_time, para_init = generate_data(x0,N)
+plot_survival(range(0,1,100), size[8], para_init[1], para_init[2], para_init[3], para_init[4])
+
+log_likeli(div_time,size,para_init[1],para_init[2],para_init[3], para_init[4])
 
 #define prior Distributions
-# u = LogNormal(log(2)-1^2/2,1);
-u = LogNormal(log(2)-1^2/2,1);
-v = LogNormal(log(2)-1^2/2,1);
-omega1 = LogNormal(log(2)-1^2/2,1);
-omega2 = LogNormal(log(2)-1^2/2,1);
+u = LogNormal(log(0.8)-1^2/2,1); # mu = 0.8, sigma = 1
+v = LogNormal(log(0.8)-1^2/2,1);
+omega1 = LogNormal(log(0.8)-1^2/2,1);
+omega2 = LogNormal(log(0.8)-1^2/2,1);
 para = Distribution[omega1,omega2,u,v]
 
+# plot(u, xlim=(0,10), ylim=(0, 2), yflip = false)
 
 #applying the MH algo for the posterior Distribution
-p = SimpleMCMC.MetropolisHastings(div_time, size, para, log_likeli, samples = 50000, burnedinsamples = 1000)
+p = SimpleMCMC.MetropolisHastings(div_time, size, para, log_likeli, samples = 10000, burnedinsamples = 1000)
 SimpleMCMC.describe_paramvec("omega1","omega1",vec(p[:,1]))
 SimpleMCMC.describe_paramvec("omega2","omega2",vec(p[:,2]))
 SimpleMCMC.describe_paramvec("u","u",vec(p[:,3]))
 SimpleMCMC.describe_paramvec("v","v",vec(p[:,4]))
 
 corrplot(p[:,3:4])
+
+
+
 
