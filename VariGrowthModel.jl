4cb8ca7b208f42ac1cdcf1200b2f8cea65328a23
Jonas <jonas.guler@uzh.ch>
2022-03-09 15:22:40 +0100

    adding log_posterior and log_prior

--- VariGrowthModel.jl
+++ VariGrowthModel.jl
@@ -1,36 +1,27 @@
-using Roots, Distributions, Statistics, StatsPlots,Plots
-include("./SimpleMCMC.jl")
-using .SimpleMCMC
+using Roots, Distributions, Statistics, StatsPlots, Plots, AffineInvariantMCMC
 
 
 function generate_data(s,N)
 
-    u_init = 0.6;
-    v_init = 2.0;
-    o1_init = 1.0;
-    o2_init = 0.5;
-    sigma_init = 0.1;
-    init_para = [o1_init,o2_init,u_init,v_init,sigma_init];
-
     X = Float64[s];
     Y = Array{Float64}(undef,N,2);
     k = rand(Uniform(0,1),N);
-    alpha = rand(LogNormal(log(o1_init)-sigma_init^2/2,sigma_init),N);
+    alpha = rand(LogNormal(o1,sig),N);
     Y[:,2] = alpha
     for n = 1:N
-        if X[n] < u_init
-            t0 = 1/alpha[n]*log(u_init/X[n]);
-            f = t -> log(k[n]) + o2_init/(v_init+u_init)*((u_init)/alpha[n]*exp(alpha[n]*t) + v_init*t - u_init/alpha[n])
+        if X[n] < u
+            t0 = 1/alpha[n]*log(u/X[n]);
+            f = t -> log(k[n]) + o2/(v+u)*(u/alpha[n]*exp(alpha[n]*t) + v*t - u/alpha[n])
         else
-            t0 = 0
-            f = t -> log(k[n]) + o2_init/(v_init+u_init)*(X[n]/alpha[n]*exp(alpha[n]*t) + v_init*t - X[n]/alpha[n])
+            t0 = 0;
+            f = t -> log(k[n]) + o2/(v+u)*(X[n]/alpha[n]*exp(alpha[n]*t) + v*t - X[n]/alpha[n])
         end
         fx = ZeroProblem(f, 1)
         Y[n,1] = solve(fx)+t0;
         next_size = (X[n] * exp(alpha[n]*Y[n,1]))/2
         push!(X, next_size)
     end
-    return (X,Y,init_para)
+    return (Y,X)
 end
 
 
@@ -38,24 +29,49 @@ function plot_data(Y,X)
     t = Array{Float64}(undef,N*10);
     result = Array{Float64}(undef,N*10);
     for k = 1:N
-        temp = Array{Float64}(undef,10);
-        for j = 1:10
-            temp[j] = X[k]*exp(Y[k,2]*(j*Y[k,1])/10) 
-        end
         start = sum(Y[1:(k-1),1]);
-        t[(k-1)*10+1:k*10] = range(start,start+Y[k],10)
-        result[(k-1)*10+1:k*10] = temp
+        t[(k-1)*10+1:k*10] = range(start,start+Y[k,1],10)
+        result[(k-1)*10+1:k*10] = X[k] .* exp.(Y[k,2]*range(0,Y[k,1],10))
     end
     plot(t,result)
 end
 
 
+function log_posterior(Y,X,para::Vector)
+    if any(x->x.<0,para)
+        return -Inf
+    else
+        like = 0.;
+        for k = 1:size(Y,1)
+            if X[k] < para[3]
+                t0 = 1/para[1]*log(para[3]/X[k]);
+                temp = 0
+            else
+                temp = 0
+            end
+            like += log(temp)
+        end
+        return like + sum([logpdf(pri,para[k]) for k=1:length(para)])
+    end
+end
 
 
+function log_prior(para::Vector)
+    return sum([logpdf(pri,para[k]) for k=1:length(para)])
+end
 
 
-N = 10;
-size_init = 0.725;
+# Initial parameters
+const N = 10; #number of observations
+const m0 = 8.; #initial mass of cell
 
-mass,data,para_init = generate_data(size_init,N)
+const u = 0.6; #lower treshhold for division
+const v = 2.; #upper treshhold for division
+const o1 = 1.; #mean of growth rate distribution
+const sig = 0.1; #sd of growth rate distribution
+const o2 = 0.5; #hazard rate functions constant
+
+const pri = Uniform(2,1); #prior distribution with mean 2 and sd 1
+
+data, mass = generate_data(m0,N);
 plot_data(data,mass)
