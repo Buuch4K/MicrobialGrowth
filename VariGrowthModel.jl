a96704da1308ef383d51ab3791ad883e8bacb58e
Jonas <jonas.guler@uzh.ch>
2022-03-17 10:57:41 +0100

    doing the first samples with 1to4 parameters

--- VariGrowthModel.jl
+++ VariGrowthModel.jl
@@ -6,8 +6,8 @@ struct Data
     mass
 end
 
-function generate_data(s::Float64,num::Int64)
-    X = Float64[s];
+function generate_data(si::Float64,num::Int64)
+    X = Float64[si];
     Y = Array{Float64}(undef,num);
     k = rand(Uniform(0,1),num);
     alpha = rand(LogNormal(o1,sig),num);
@@ -30,50 +30,50 @@ end
 
 function read_data(lineage::Float64,filename::String)
     data = CSV.File(filename,select=["lineage_ID", "generationtime","length_birth","growth_rate"]);
-    s = Float64[]; t = Float64[]; g = Float64[];
+    X = Float64[]; Y = Float64[]; alpha = Float64[];
     for k = 1:length(data.lineage_ID)
         if data.lineage_ID[k] == lineage
-            push!(g, data.growth_rate[k])
-            push!(t, data.generationtime[k])
-            push!(s,data.length_birth[k])
+            push!(alpha, data.growth_rate[k])
+            push!(Y, data.generationtime[k])
+            push!(X, data.length_birth[k])
         end
     end
-    return Data(t,g,s)
+    return Data(Y,alpha,X)
 end
 
 
-function plot_data(S::Data)
-    n = length(S.time)
+function plot_data(D::Data)
+    n = length(D.time)
     t = Array{Float64}(undef,n*10);
     result = Array{Float64}(undef,n*10);
     for k = 1:n
-        start = sum(S.time[1:(k-1)]);
-        t[(k-1)*10+1:k*10] = range(start,start+S.time[k],10)
-        result[(k-1)*10+1:k*10] = S.mass[k] .* exp.(S.growth[k]*range(0,S.time[k],10))
+        start = sum(D.time[1:(k-1)]);
+        t[(k-1)*10+1:k*10] = range(start,start+D.time[k],10)
+        result[(k-1)*10+1:k*10] = D.mass[k] .* exp.(D.growth[k]*range(0,D.time[k],10))
     end
     plot(t,result)
 end
 
 
-function log_posterior(S::Data,para::Vector)
+function log_posterior(D::Data,para::Vector)
     if any(x->x.<0,para)
         return -Inf
     else
         like = 0.;
-        for k = 1:length(S.time)
-            if S.mass[k] < para[3]
-                t0 = 1/S.growth[k]*log(para[3]/S.mass[k]);
-                if S.time[k] < t0
+        for k = 1:length(D.time)
+            if D.mass[k] < para[3]
+                t0 = 1/D.growth[k]*log(para[3]/D.mass[k]);
+                if D.time[k] < t0
                     return -Inf
                 else
-                    temp = log((para[4]*para[2])/(para[4]+para[3]) + (S.mass[k]*para[2])/(para[4]+para[3])*exp(s.growth[k]*S.time[k])) + ((para[2]/(para[4]+para[3]))*(para[3]/S.growth[k] - (S.mass[k]*exp(S.growth[k]*S.time[k]))/S.growth[k] - para[4]*S.time[k] + para[4]*t0))
+                    temp = log((para[4]*para[2])/(para[4]+para[3]) + (D.mass[k]*para[2])/(para[4]+para[3])*exp(D.growth[k]*D.time[k])) + ((para[2]/(para[4]+para[3]))*(para[3]/D.growth[k] - (D.mass[k]*exp(D.growth[k]*D.time[k]))/D.growth[k] - para[4]*D.time[k] + para[4]*t0))
                 end
             else
-                temp = log((para[4]*para[2])/(para[4]+para[3]) + (S.mass[k]*para[2])/(para[4]+para[3])*exp(S.growth[k]*S.time[k])) + ((para[2]/(para[4]+para[3]))*(S.mass[k]/S.growth[k] - (S.mass[k]*exp(S.growth[k]*S.time[k]))/S.growth[k] - para[4]*S.time[k]))
+                temp = log((para[4]*para[2])/(para[4]+para[3]) + (D.mass[k]*para[2])/(para[4]+para[3])*exp(D.growth[k]*D.time[k])) + ((para[2]/(para[4]+para[3]))*(D.mass[k]/D.growth[k] - (D.mass[k]*exp(D.growth[k]*D.time[k]))/D.growth[k] - para[4]*D.time[k]))
             end
             like += temp
         end
-        return like + sum([logpdf(pri,para[k]) for k=1:length(para)])
+        return like + sum([logpdf(LogNormal(para[1],para[5]),D.growth[k]) for k=1:length(D.growth)]) + sum([logpdf(pri,para[k]) for k=1:length(para)])
     end
 end
 
@@ -83,20 +83,35 @@ function log_prior(para::Vector)
 end
 
 
-# Initial parameters
-const N = 10; #number of observations
-const m0 = 8.; #initial mass of cell
-
+# initial parameters
 const u = 0.6; #lower treshhold for division
 const v = 2.; #upper treshhold for division
 const o1 = 1.; #mean of growth rate distribution
 const sig = 0.5; #sd of growth rate distribution
 const o2 = 0.5; #hazard rate functions constant
 
-const pri = Uniform(0,10); #prior distribution with mean 2 and sd 1
+const pri = Uniform(0,10); #prior distribution
 
-gendata = generate_data(m0,N);
-# realdata = read_data(15.,"data/Susman18_physical_units.csv")
+generate = true;
+if generate
+    # Initial variables
+    N = 10; #number of observations
+    m0 = 8.; #initial mass of cell
+
+    gendata = generate_data(m0,N);
+else
+    readdata = read_data(15.,"data/Susman18_physical_units.csv");
+end
 plot_data(gendata)
+
+# applying the MH algo for the posterior Distribution
+numdims = 4; numwalkers = 20; thinning = 10; numsamples_perwalker = 20000; burnin = 1000;
+loglhood = x -> log_posterior(gendata,[x[1],x[2],x[4],v,x[3]]);
+
+x = rand(pri,numdims,numwalkers); # define initial points for parameters
+chain, llhoodvals = AffineInvariantMCMC.sample(loglhood,numwalkers,x,burnin,1);
+chain, llhoodvals = AffineInvariantMCMC.sample(loglhood,numwalkers,chain[:, :, end],numsamples_perwalker,thinning);
+flatchain, flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(chain,llhoodvals);
+
 
 
