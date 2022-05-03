3af62bbb8297418680045edbae91c5292cc5c08a
Jonas <jonas.guler@uzh.ch>
2022-05-03 16:28:41 +0200

    Changing inference in to steps: first growth and division distribution then rest

--- ProteinModel.jl
+++ ProteinModel.jl
@@ -45,55 +45,80 @@ function plot_data(D::Data)
 end
 
 
-function log_likeli(D::Data,p::Vector)
-    # p = [o1,sig,b1,b2,o2,u,v,c]
+function log_likeli_gd(p::Vector,D::Data)
+    # p = [o1,sig,b1,b2]
+    if any(x->x.<0,p)
+        return -Inf
+    else
+        return sum([logpdf(Gamma(p[1],p[2]),D.growth[k]) for k = 1:length(D.growth)]) + sum([logpdf(Beta(p[3],p[4]),D.divratio[k]) for k = 1:length(D.divratio)])
+    end
+end
+
+
+function log_prior_gd(p::Vector)
+    # p = [o1,sig,b1,b2]
+    return logpdf(pri_Gamma,p[1]) + logpdf(pri,p[2]) + logpdf(pri_Beta,p[3]) + logpdf(pri_Beta,p[4])
+end
+
+
+function log_likeli(p::Vector,D::Data,f::Vector)
+    # p = [o2,u,v,c], f = [o1,sig,b1,b2]
     if any(x->x.<0,p)
         return -Inf
     else
         like = 0.;
         for k = 1:length(D.time)
-            t0 = max(0,1/D.growth[k]*log((p[6]+p[8]*D.mass[k])/(p[8]*D.mass[k])))
+            t0 = max(0,1/D.growth[k]*log((p[2]+p[4]*D.mass[k])/(p[4]*D.mass[k])))
             if D.time[k] < t0
                 return -Inf
             else
-                temp = log(p[5]/(p[6]+p[7])*(p[7]+p[8]*D.mass[k]*(exp(D.growth[k]*D.time[k])-1))) + (-p[5]/(p[6]+p[7])*((p[8]*D.mass[k])/D.growth[k]*(exp(D.growth[k]*D.time[k]) - exp(D.growth[k]*t0)) + (p[7]-p[8]*D.mass[k])*D.time[k] + (p[8]*D.mass[k]-p[7])*t0))
+                temp = log(p[1]/(p[2]+p[3])*(p[3]+p[4]*D.mass[k]*(exp(D.growth[k]*D.time[k])-1))) + (-p[1]/(p[2]+p[3])*((p[4]*D.mass[k])/D.growth[k]*(exp(D.growth[k]*D.time[k]) - exp(D.growth[k]*t0)) + (p[3]-p[4]*D.mass[k])*D.time[k] + (p[4]*D.mass[k]-p[3])*t0))
             end
             like += temp
         end
-        return like + sum([logpdf(Gamma(p[1],p[2]),D.growth[k]) for k = 1:length(D.growth)]) + sum([logpdf(Beta(p[3],p[4]),D.divratio[k]) for k = 1:length(D.divratio)])
+        return like + sum([logpdf(Gamma(f[1],f[2]),D.growth[k]) for k = 1:length(D.growth)]) + sum([logpdf(Beta(f[3],f[4]),D.divratio[k]) for k = 1:length(D.divratio)])
     end
 end
 
 
 function log_prior(p::Vector)
-    # p = [o1,sig,b1,b2,o2,u,v,c]
-    if p[6] > p[7]
+    # p = [o2,u,v,c]
+    if p[2] > p[3]
         return -Inf
     else
-        gam = logpdf(pri_Gamma,para[1]) + logpdf(pri,para[2]);
-        be = logpdf(pri_Beta,para[3]) + logpdf(pri_Beta,para[4]);
-        re = sum([logpdf(pri,para[k]) for k=5:length(p)]);
-        return gam+be+re
+        return sum([logpdf(pri,p[k]) for k=1:length(p)])
     end
 end
 
 
+function remove_stuck_chain(chain,llhood,nwalk)
+    bad_idx = []; 
+    for k=1:nwalk
+        if all(y -> y == first(chain[2,k,:]), chain[2,k,:])
+            push!(bad_idx, k);
+        end
+    end
+    idx = setdiff(1:nwalk,bad_idx)
+    return chain[:,idx,:],llhood[:,idx,:]
+end
+
+
 # initial parameters
-const o1 = 27.1232; # growth distribution
+const o1 = 27.; # growth distribution
 const sig = 0.05189;
 
-const b1 = 16.0817; # division distribution
-const b2 = 16.0425;
+const b1 = 16.; # division distribution
+const b2 = 16.;
 
 const o2 = 1.7761; #hazard ratio
-const u = 2.5; #lower treshhold for division
+const u = 0.2; #lower treshhold for division
 const v = 5.5; #upper treshhold for division
 const c = 1.; #protein constant
 
 # prior distributions
-const pri_Gamma = Uniform(20,34);
-const pri_Beta = Uniform(10,22);
-const pri = Uniform(0,6);
+pri_Gamma = Uniform(17,37);
+pri_Beta = Uniform(6,26);
+pri = Uniform(0,6);
 
 # generate data using defined model
 N = 200; #number of observations
@@ -105,17 +130,25 @@ readdata = read_data("data/modified_Susman18_physical_units.csv")
 
 plot_data(gendata)
 
-# applying the MH algo for the posterior Distribution
-numdims = 2; numwalkers = 20; thinning = 10; numsamples_perwalker = 20000; burnin = 1000;
-logpost = x -> log_likeli(gendata,[x[1],x[2],b1,b2,o2,u,v,c]) + log_prior([x[1],x[2],b1,b2,o2,u,v,c]);
+# applying the MH algo for the posterior Distribution in two steps
+numdims = 4; numwalkers = 20; thinning = 10; numsamples_perwalker = 20000; burnin = 1000;
+logpost_gd = x -> log_likeli_gd(x,gendata) + log_prior_gd(x);
 
-x = rand(pri,numdims,numwalkers); # define initial points with all same prior
-chain, llhoodvals = AffineInvariantMCMC.sample(logpost,numwalkers,x,burnin,1);
-chain, llhoodvals = AffineInvariantMCMC.sample(logpost,numwalkers,chain[:, :, end],numsamples_perwalker,thinning);
-flatchain, flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(chain,llhoodvals);
+# step one: Infer parameters for growth and division distribution
+x = vcat(rand(pri_Gamma,1,numwalkers),rand(pri,1,numwalkers),rand(pri_Beta,2,numwalkers)); # define initial points with all same prior
+chain1, llhoodvals1 = AffineInvariantMCMC.sample(logpost_gd,numwalkers,x,burnin,1);
+chain1, llhoodvals1 = AffineInvariantMCMC.sample(logpost_gd,numwalkers,chain1[:, :, end],numsamples_perwalker,thinning);
+flatchain1, flatllhoodvals1 = AffineInvariantMCMC.flattenmcmcarray(chain1,llhoodvals1);
+flatchain1 = permutedims(flatchain1,[2,1]);
+fixed = mean(flatchain1,dims=1)[1,:]
 
-# permute dimensions to simplify plotting
-chain = permutedims(chain, [1,3,2]);
-flatchain = permutedims(flatchain,[2,1]);
+# step two: Infer the parameters o2,u,v,c
+numdims = 4; numsamples_perwalker = 80000; logpost = x -> log_likeli(x,gendata,fixed) + log_prior(x);
+x = rand(pri,numdims,numwalkers);
+chain2, llhoodvals2 = AffineInvariantMCMC.sample(logpost,numwalkers,x,burnin,1);
+chain2, llhoodvals2 = AffineInvariantMCMC.sample(logpost,numwalkers,chain2[:, :, end],numsamples_perwalker,thinning);
+flatchain2, flatllhoodvals2 = AffineInvariantMCMC.flattenmcmcarray(chain2,llhoodvals2);
 
-corrplot(flatchain)
+mod_chain2, mod_llhoodvals2 = remove_stuck_chain(chain2,llhoodvals2,numwalkers);
+mod_flatchain2, mod_flatllhoodvals2 = AffineInvariantMCMC.flattenmcmcarray(mod_chain2,mod_llhoodvals2);
+mod_flatchain2 = permutedims(mod_flatchain2,[2,1]);
