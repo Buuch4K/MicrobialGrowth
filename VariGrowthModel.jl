82c714ceb7df46af7512049efeb30f6ea5e08029
Jonas <jonas.guler@uzh.ch>
2022-05-24 10:25:41 +0200

    changing prior for beta to uniform 0.4,0.6 and 0,0.2

--- VariGrowthModel.jl
+++ VariGrowthModel.jl
@@ -77,7 +77,7 @@ function log_prior(p::Vector)
     if p[6] >= p[7]
         return -Inf
     else
-        return sum([logpdf(pri_gamma,p[k]) for k=1:2]) + sum([logpdf(pri_beta,p[k]) for k=3:4]) + sum([logpdf(pri,p[k]) for k=5:length(p)])
+        return sum([logpdf(pri_gamma,p[k]) for k=1:2]) + logpdf(pri_beta1,p[3]) + logpdf(pri_beta2,p[4]) + sum([logpdf(pri,p[k]) for k=5:length(p)])
     end
 end
 
@@ -95,6 +95,16 @@ function remove_stuck_chain(chain,llhood,nwalk::Int64)
 end
 
 
+function extract_beta!(flat::Matrix)
+    for i = 1:size(flat)[2]
+        k1 = flat[3,i]; k2 = flat[4,i];
+        flat[3,i] = k1/(k1+k2)
+        flat[4,i] = (k1*k2)/((k1+k2)^2*(k1+k2+1))
+    end
+    return flat
+end
+
+
 # initial parameters
 const o1 = 1.405; # growth distribution
 const sig = 0.07;
@@ -107,9 +117,10 @@ const u = 0.2; #lower treshhold for division
 const v = 4.; #upper treshhold for division
 
 #prior distributions
-pri_gamma = Uniform(0,5);
-pri_beta = Uniform(0,1);
-pri = Uniform(0,14);
+pri_gamma = Uniform(0,3);
+pri_beta1 = Uniform(0.4,0.6);
+pri_beta2 = Uniform(0,0.2);
+pri = Uniform(0,5);
 
 # generate data using defined model
 N = 249; #number of observations
@@ -124,10 +135,10 @@ plot_data(readdata)
 scatter(gendata.divratio .* exp.(gendata.growth[2:end].*gendata.time[2:end]))
 
 # applying the MH algo for the posterior Distribution
-numdims = 6; numwalkers = 20; thinning = 10; numsamples_perwalker = 40000; burnin = 2000;
-logpost = x -> log_likeli(vcat(x,v),readdata) + log_prior(vcat(x,v));
+numdims = 7; numwalkers = 20; thinning = 10; numsamples_perwalker = 40000; burnin = 2000;
+logpost = x -> log_likeli(x,readdata) + log_prior(x);
 
-x = vcat(rand(pri_gamma,2,numwalkers),rand(pri_beta,2,numwalkers),rand(pri,numdims-4,numwalkers));
+x = vcat(rand(pri_gamma,2,numwalkers),rand(pri_beta1,1,numwalkers),rand(pri_beta2,1,numwalkers),rand(pri,numdims-4,numwalkers));
 
 chain, llhoodvals = AffineInvariantMCMC.sample(logpost,numwalkers,x,burnin,1);
 chain, llhoodvals = AffineInvariantMCMC.sample(logpost,numwalkers,chain[:, :, end],numsamples_perwalker,thinning);
@@ -135,3 +146,4 @@ flatchain, flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(chain,llhoodval
 
 mod_chain, mod_llhoodvals = remove_stuck_chain(chain,llhoodvals,numwalkers);
 mod_flatchain, mod_flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(mod_chain,mod_llhoodvals);
+# extract_beta!(mod_flatchain);
