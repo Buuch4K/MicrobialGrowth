5c73cefa0e41493b7754d09ea64d988c95ea26c8
Jonas <jonas.guler@uzh.ch>
2022-06-10 10:45:12 +0200

    increase tickfont and correct label to beta

--- PreparePlots.jl
+++ PreparePlots.jl
@@ -1,4 +1,4 @@
-using Roots, Distributions, Statistics, StatsPlots, Plots, AffineInvariantMCMC, CSV, JLD2
+using Roots, Distributions, Statistics, StatsPlots, Plots, AffineInvariantMCMC, CSV, JLD2, DataFrames
 
 struct Data
     time
@@ -27,8 +27,9 @@ function generate_data(si::Float64,num::Int64)
         next_size = X[n] * exp(alpha[n]*Y[n]) * f[n]
         push!(X, next_size)
     end
-    return Data(Y,alpha,X[1:num],f)
+    return Data(Y,alpha,X[1:num],f[1:num])
 end
+
 
 readdata = PMreal_data;
 ex_read = Array{Float64}(undef,length(readdata.time),3);
@@ -36,7 +37,7 @@ ex_read[:,1] = vcat(readdata.divratio,0.49);
 ex_read[:,2] = readdata.growth;
 ex_read[:,3] = readdata.time;
 
-corrplot(ex_read,label=["f","alpha","tau"],tickfontsize=4,guidefontsize=6)
+corrplot(ex_read,label=["beta","alpha","tau"],tickfontsize=5,guidefontsize=6)
 
 const o1,sig,b1,b2,o2,u,v = PMreal_flatchain[:,argmax(PMreal_flatllhood)]; const c=1;
 gendata = generate_data(2.6,2500);
@@ -45,19 +46,19 @@ ex_gen[:,1] = gendata.divratio;
 ex_gen[:,2] = gendata.growth;
 ex_gen[:,3] = gendata.time;
 
-corrplot(ex_gen,label=["f","alpha","tau"],tickfontsize=4,guidefontsize=6)
+corrplot(ex_gen,label=["beta","alpha","tau"],tickfontsize=5,guidefontsize=6)
 
 
 # scatter the three important data values to see their dependencies
 scatter(readdata.divratio,readdata.growth,readdata.time,label="read");
 scatter!(gendata.divratio,gendata.growth,gendata.time,label="synthetic");
-plot!(xlabel="f",ylabel="alpha",zlabel="tau")
-
-# scatter the claimed formula between f, alpha and tau.
-scatter(gendata.divratio .* exp.(gendata.growth[2:end].*gendata.time[2:end]),label=false)
-
-
+plot!(xlabel="beta",ylabel="alpha",zlabel="tau")
 
 # save the real world and synthetic data with mle estimates to a csv file
 CSV.write("pmsyn.csv",DataFrame(f=ex_gen[:,1],alpha=ex_gen[:,2],tau=ex_gen[:,3]));
 CSV.write("pmreal.csv",DataFrame(f=ex_read[:,1],alpha=ex_read[:,2],tau=ex_read[:,3]));
+
+
+################ export all flatchains for construct pairplots in python #####################
+flatchain = transpose(PMsyn_flatchain);
+CSV.write("pmsyn_flatchain.csv",DataFrame(mu_a=flatchain[:,1],sigma_a=flatchain[:,2],mu_b=flatchain[:,3],sigma_b=flatchain[:,4],o2=flatchain[:,5],u=flatchain[:,6],v=flatchain[:,7]));
